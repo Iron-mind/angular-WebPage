306411cc370e50ac8c990ee690d5c631daf01c92
Juan David Iron-mind <davidtovarmontoya@gmail.com>
2022-07-28 17:44:49 -0500

    structuras de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './component.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  products: Product[]=[{
+    id: Math.random(),
+    name: 'camisa',
+    price: 10,
+    description: ' camisa de algodon',
+    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+  },
+  {
+    id: Math.random(),
+    name: 'pantalon',
+    price: 20,
+    description: ' pantalon de algodon',
+    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+  },
+  {
+    id: Math.random(),
+    name:'curso de programación',
+    price: 30,
+    description: ' curso de programación',
+    image:'https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg?v=47faa659a05e'
+  },
+  {
+    id: Math.random(),
+    name:'curso de programación',
+    price: 30,
+    description: ' curso de programación',
+    image:'https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg?v=47faa659a05e'
+  },
+  {
+    id: Math.random(),
+    name:'curso de programación',
+    price: 30,
+    description: ' curso de programación',
+    image:'https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg?v=47faa659a05e'
+  }];
+  product: Product={
+    id: Math.random(),
+    name: '',
+    price: 0,
+    description: '',
+    image: ''
+  };
+  user={
+    name: 'Juan',
+  }
+
+  addProduct(){
+    this.products.push(this.product);
+    this.product={
+      id: Math.random(),
+      name: '',
+      price: 0,
+      description: '',
+      image: ''
+    };
+  }
+
 }
