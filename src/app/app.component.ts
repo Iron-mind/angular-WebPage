853e2a7b10a6c215980a8922424b02c7e02c03d7
Juan David Iron-mind <davidtovarmontoya@gmail.com>
2022-07-30 21:31:06 -0500

    ngFor con inputs padres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Product } from './component.model';
+import { Product } from './models/product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,28 @@ import { Product } from './component.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title= 'my-store'
    imgParent='';
+   products:Product[] =[{
+    name: 'Product 1',
+    price: 333,
+    description:'Eso es un producto',
+    image: './assets/imgs/product-1.jpg'
+   },
+    {
+    name: 'Product 2',
+    price: 4000,
+    description:' Eso es otro producto',
+    image: './assets/imgs/product-1.jpg'
+    },
+    {
+      name: 'Product 2',
+      price: 4000,
+      description:' Eso es otro producto',
+      image: './assets/imgs/product-1.jpg'
+    }
+    ]
+
    onLoadImg(msg:string){
     console.log('Imagen cargada, alert desde el padre. '+msg);
    }
